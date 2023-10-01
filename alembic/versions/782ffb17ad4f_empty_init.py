358a4da085a5e11f373be5e9111536e9c62c6fbd
Kelvin Mue <kelvinmue@gmail.com>
2023-10-01 20:53:35 +0300

    Completed

---
+++ alembic/versions/782ffb17ad4f_empty_init.py
@@ -0,0 +1,24 @@
+"""empty init
+
+Revision ID: 782ffb17ad4f
+Revises: 
+Create Date: 2023-10-01 20:43:46.183611
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '782ffb17ad4f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ alembic/versions/de2a8d3b237d_empty_init.py
@@ -0,0 +1,24 @@
+"""empty init
+
+Revision ID: de2a8d3b237d
+Revises: 782ffb17ad4f
+Create Date: 2023-10-01 20:45:28.775347
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'de2a8d3b237d'
+down_revision = '782ffb17ad4f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
